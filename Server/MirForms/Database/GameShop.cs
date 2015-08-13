31c238561c3a1c54c2b20d25fdf55e93ac3a7de4
Elijah <ash@audis.co.uk>
2015-08-13 23:58:00 +0100

    Gameshop Start

---
+++ Server/MirForms/Database/GameShop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Server.MirForms.Database
+{
+    public partial class GameShop : Form
+    {
+        public GameShop()
+        {
+            InitializeComponent();
+        }
+    }
+}
